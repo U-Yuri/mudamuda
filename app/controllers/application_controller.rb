bad8c5c88dbdc915346e41c927b2cc3c6941969e
yuriririn <80314978+yuriririn@users.noreply.github.com>
2024-02-20 16:58:06 +0900

    ログイン画面のパス変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    mudamuda_path
+  end
+
+  def after_sign_in_path_for(resource)
+    mudamuda_path
+  end
+
 end
