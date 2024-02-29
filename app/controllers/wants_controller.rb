221ea9d5a2f4cc3d0e290d1401fff885858f9807
yuriririn <80314978+yuriririn@users.noreply.github.com>
2024-02-29 17:17:08 +0900

    ログインしている人はマイページに、していない人はログイン画面に誘導

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -1,5 +1,9 @@
 class WantsController < ApplicationController
-  def top
-    render "wants/my_page"
+  def my_page
+    if user_signed_in?
+      render "wants/my_page"
+    else
+      redirect_to "/users/sign_in"
+    end
   end
 end
