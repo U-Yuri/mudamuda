4475b25c554e36ded2cf874ff451a6dc622f0827
yuriririn <80314978+yuriririn@users.noreply.github.com>
2024-03-01 15:06:30 +0900

    my_pageにwants一覧が表示できるようにした(userごとではなくとりあえずみんなが登録した全てが見れる状態)

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -1,12 +1,12 @@
 class WantsController < ApplicationController
   def my_page
+    @wants = Want.all
+
     if user_signed_in?
       render "wants/my_page"
     else
       redirect_to "/users/sign_in"
     end
-
-    @wants = Want.all
   end
 
   def new
